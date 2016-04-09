1f67c360c8b42b6475acf106496d25673b98230f
Hallison Boaventura <hallisonboaventura@gmail.com>
2016-04-09 18:08:37 -0300

    Monitoramento do valor contido no endereço local
    A cada 2 segundos.

--- target.c
+++ target.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <unistd.h>
+
 typedef struct {
   unsigned int mana;
 } player_t;
@@ -5,4 +8,11 @@ typedef struct {
 // 0x601044
 player_t player;
 
-void main() {}
+void main() {
+  player.mana = 104;
+
+  while(1) {
+    printf("Current player mana is %u\n", player.mana);
+    usleep(2000000);
+  }
+}
