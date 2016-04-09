d88ae0f88de988c39d5a87b2b71ad70a19de993e
Hallison Boaventura <hallisonboaventura@gmail.com>
2016-04-09 19:13:21 -0300

    attach & detach
    Para que o attach seja executado com sucesso, é necessário
    que o usuário que execute tenha privilégios para isso.
    
    Se houverem erros, serão mostrados.

--- ptrace.c
+++ ptrace.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/ptrace.h>
 
 #define MANA_ADDR 0x601044
 
@@ -7,6 +13,19 @@ int main(int argc, char **argv)
   if (argc != 2) {
     printf("usage:\nptrace %%u <pid>\n");
     return 1;
+  }
+
+  // Entrada do usuário não verificada
+  pid_t pid = strtol(argv[1], 0, 10);
+
+  if (ptrace(PTRACE_ATTACH, pid, 0, 0) == -1) {
+    fprintf(stderr, "%s\n", strerror(errno));
+    return 2;
+  }
+
+  if (ptrace(PTRACE_DETACH, pid, 0, 0) == -1) {
+    fprintf(stderr, "%s\n", strerror(errno));
+    return 3;
   }
 
   return 0;
