fd26bf9608b1742c6db897efb076f66604c63781
Hallison Boaventura <hallisonboaventura@gmail.com>
2016-05-24 00:31:10 -0300

    Dois espaços por quatro espaços na identação

--- ptrace.c
+++ ptrace.c
@@ -11,39 +11,39 @@
 
 int main(int argc, char **argv)
 {
-  if (argc != 2) {
-    printf("usage:\nptrace %%u <pid>\n");
-    return 1;
-  }
+    if (argc != 2) {
+        printf("usage:\nptrace %%u <pid>\n");
+        return 1;
+    }
 
-  // Entrada do usuário não verificada
-  pid_t pid = strtol(argv[1], 0, 10);
+    // Entrada do usuário não verificada
+    pid_t pid = strtol(argv[1], 0, 10);
 
-  if (ptrace(PTRACE_ATTACH, pid, 0, 0) == -1) {
-    fprintf(stderr, "%s\n", strerror(errno));
-    return 2;
-  }
+    if (ptrace(PTRACE_ATTACH, pid, 0, 0) == -1) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        return 2;
+    }
 
-  // Esperando tracee parar
-  wait(0);
+    // Esperando tracee parar
+    wait(0);
 
-  long current_mana = ptrace(PTRACE_PEEKDATA, pid, (void *) MANA_ADDR, 0);
-  if (current_mana == -1) {
-    fprintf(stderr, "%s\n", strerror(errno));
-    return 3;
-  }
+    long current_mana = ptrace(PTRACE_PEEKDATA, pid, (void *) MANA_ADDR, 0);
+    if (current_mana == -1) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        return 3;
+    }
 
-  current_mana += 55;
-  if (ptrace(PTRACE_POKEDATA, pid, (void *) MANA_ADDR, (void *) current_mana)) {
-    fprintf(stderr, "%s\n", strerror(errno));
-    return 4;
-  }
+    current_mana += 55;
+    if (ptrace(PTRACE_POKEDATA, pid, (void *) MANA_ADDR, (void *) current_mana)) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        return 4;
+    }
 
-  if (ptrace(PTRACE_DETACH, pid, 0, 0) == -1) {
-    fprintf(stderr, "%s\n", strerror(errno));
-    return 5;
-  }
+    if (ptrace(PTRACE_DETACH, pid, 0, 0) == -1) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        return 5;
+    }
 
-  printf("Done!\n");
-  return 0;
+    printf("Done!\n");
+    return 0;
 }
--- readv-writev.c
+++ readv-writev.c
@@ -17,52 +17,52 @@ typedef struct iovec iovec_t;
 
 ssize_t remote_read_bytes(pid_t pid, void *local_addr, void *remote_addr, size_t len)
 {
-  iovec_t local_iov  = {local_addr,  len};
-  iovec_t remote_iov = {remote_addr, len};
-  return process_vm_readv(pid, &local_iov, 1, &remote_iov, 1, 0);
+    iovec_t local_iov  = {local_addr,  len};
+    iovec_t remote_iov = {remote_addr, len};
+    return process_vm_readv(pid, &local_iov, 1, &remote_iov, 1, 0);
 }
 
 ssize_t remote_write_bytes(pid_t pid, void *local_addr, void *remote_addr, size_t len)
 {
-  iovec_t local_iov  = {local_addr,  len};
-  iovec_t remote_iov = {remote_addr, len};
-  return process_vm_writev(pid, &local_iov, 1, &remote_iov, 1, 0);
+    iovec_t local_iov  = {local_addr,  len};
+    iovec_t remote_iov = {remote_addr, len};
+    return process_vm_writev(pid, &local_iov, 1, &remote_iov, 1, 0);
 }
 
 ssize_t remote_read32(pid_t pid, void *local_addr, void *remote_addr)
 {
-  return remote_read_bytes(pid, local_addr, remote_addr, 4);
+    return remote_read_bytes(pid, local_addr, remote_addr, 4);
 }
 
 ssize_t remote_write32(pid_t pid, void *local_addr, void *remote_addr)
 {
-  return remote_write_bytes(pid, local_addr, remote_addr, 4);
+    return remote_write_bytes(pid, local_addr, remote_addr, 4);
 }
 
 int main(int argc, char **argv)
 {
-  if (argc != 2) {
-    printf("usage:\nreadv-writev %%u <pid>\n");
-    return 1;
-  }
+    if (argc != 2) {
+        printf("usage:\nreadv-writev %%u <pid>\n");
+        return 1;
+    }
 
-  // Entrada do usuário não é verificada
-  pid_t pid = strtol(argv[1], 0, 10);
+    // Entrada do usuário não é verificada
+    pid_t pid = strtol(argv[1], 0, 10);
 
-  unsigned int current_mana;
-  ssize_t bytes_read = remote_read32(pid, &current_mana, (void *) MANA_ADDR);
-  if (bytes_read != sizeof current_mana) {
-    fprintf(stderr, "%s\n", strerror(errno));
-    return 2;
-  }
+    unsigned int current_mana;
+    ssize_t bytes_read = remote_read32(pid, &current_mana, (void *) MANA_ADDR);
+    if (bytes_read != sizeof current_mana) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        return 2;
+    }
 
-  current_mana += 301;
-  ssize_t bytes_written = remote_write32(pid, &current_mana, (void *) MANA_ADDR);
-  if (bytes_written != sizeof current_mana) {
-    fprintf(stderr, "%s\n", strerror(errno));
-    return 3;
-  }
+    current_mana += 301;
+    ssize_t bytes_written = remote_write32(pid, &current_mana, (void *) MANA_ADDR);
+    if (bytes_written != sizeof current_mana) {
+        fprintf(stderr, "%s\n", strerror(errno));
+        return 3;
+    }
 
-  printf("Done!\n");
-  return 0;
+    printf("Done!\n");
+    return 0;
 }
